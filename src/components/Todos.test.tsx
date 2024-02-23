d834cb23b5420d6e4e99174f1c1c9619fadb50bb
dsound-zz <demiansims@gmail.com>
2024-02-23 09:17:12 -0500

    wip

--- src/components/Todos.test.tsx
+++ src/components/Todos.test.tsx
@@ -18,11 +18,70 @@ describe("Todos", () => {
   it("adds a todo item", () => {
     render(<Todos />)
 
-    const input = screen.getByLabelText("todo input")
-    const button = screen.getByRole("button")
-    fireEvent.change(input, { target: { value: "Buy Groceries" } })
-    fireEvent.click(button)
+    const createInput = screen.getByLabelText("todo input")
+    const submitButton = screen.getByLabelText("submit button")
+    fireEvent.change(createInput, { target: { value: "Buy Groceries" } })
+    fireEvent.click(submitButton)
 
     expect(screen.getByText("Buy Groceries")).toBeInTheDocument()
+  })
+
+  it("deletes a todo item", () => {
+    render(<Todos />)
+
+    // first add the item
+    // get the input field
+    const createInput = screen.getByLabelText("todo input")
+    // get the submit button
+    const submitButton = screen.getByLabelText("submit button")
+    // add a value to the input by drilling down: { target: { value: "value "}}
+    fireEvent.change(createInput, { target: { value: "Buy Groceries " } })
+    fireEvent.click(submitButton)
+
+    expect(screen.getByText("Buy Groceries")).toBeInTheDocument()
+
+    // find and delete the added item "Buy Groceries"
+    const todoItem = screen.getByText("Buy Groceries")
+    // find delete button
+    const deleteButton = screen.getByLabelText("delete button")
+    // click delete button
+    fireEvent.click(deleteButton)
+
+    // expect the todo item not to be in the document
+    expect(todoItem).not.toBeInTheDocument()
+  })
+
+  it("Edits a todo", () => {
+    render(<Todos />)
+
+    // first find elements and add item on screen
+
+    // get input
+    const createInput = screen.getByLabelText("todo input")
+    // get the submit button
+    const submitButton = screen.getByLabelText("submit button")
+    // and value to input with onChange
+    fireEvent.change(createInput, { target: { value: "Buy Groceries" } })
+    // fire submit to add todo
+    fireEvent.click(submitButton)
+
+    expect(screen.getByText("Buy Groceries")).toBeInTheDocument()
+
+    // get edit button
+    const editButton = screen.getByLabelText("edit button")
+    // fire edit button
+    fireEvent.click(editButton)
+
+    // get edit input
+    const editInput = screen.getByLabelText("edit input")
+    // get submit on edit input
+    const editSubmit = screen.getByLabelText("edit submit")
+    // change input value to new value
+    fireEvent.change(editInput, { target: { input: "Buy Gas" } })
+    // click submit on edit input
+    fireEvent.click(editSubmit)
+    // expect todo to equal new value
+
+    expect(screen.getByText("Buy Gas")).toBeInTheDocument()
   })
 })
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,7 +2,12 @@ import React, { useState } from "react"
 
 const Todos = () => {
   const [text, setText] = useState<string>("")
+  const [editText, setEditText] = useState<string>("")
   const [todos, setTodos] = useState<string[] | []>([])
+  const [editing, setEditing] = useState<{
+    index: number | null
+    isEdit: boolean
+  }>({ index: null, isEdit: false })
 
   const handleInput = () => {
     if (text.trim() !== "") {
@@ -11,7 +16,23 @@ const Todos = () => {
     }
   }
 
-  console.log(text)
+  const handleEdit = (idx: number) => {
+    if (editText.trim() !== "") {
+      const todosCopy = [...todos]
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      todosCopy[idx] = editText
+      setTodos(todosCopy)
+      setEditText("")
+      setEditing({ index: null, isEdit: false })
+    }
+  }
+
+  const handleDelete = (idx: number) => {
+    const updatedTodos = todos.filter((_, index) => idx !== index)
+    setTodos(updatedTodos)
+  }
+
+  console.log(editing)
   console.log(todos)
   return (
     <div>
@@ -22,13 +43,54 @@ const Todos = () => {
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <button onClick={handleInput}>Submit</button>
+      <button aria-label='submit button' onClick={handleInput}>
+        Submit
+      </button>
 
       <div>
-        {todos.map((todo) => (
-          <ul key={todo}>
-            <li>{todo}</li>
-          </ul>
+        {todos.map((todo, idx) => (
+          <ol key={todo}>
+            {editing.index === idx && editing.isEdit ? (
+              <>
+                <input
+                  aria-label='edit input'
+                  type='text'
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                />
+                <button
+                  aria-label='edit submit'
+                  onClick={() => handleEdit(idx)}
+                >
+                  Submit
+                </button>
+              </>
+            ) : (
+              <li aria-label={todo}>
+                {todo}
+
+                <span>
+                  <button
+                    aria-label='edit button'
+                    style={{ marginLeft: "30px" }}
+                    onClick={() => {
+                      setEditing({ index: idx, isEdit: true })
+                      setEditText(todo)
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    aria-label='delete button'
+                    style={{ marginLeft: "30px" }}
+                    onClick={() => handleDelete(idx)}
+                  >
+                    delete
+                  </button>
+                </span>
+              </li>
+            )}
+          </ol>
         ))}
       </div>
     </div>
